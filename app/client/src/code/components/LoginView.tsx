61bf0c2081cfba6fe98b5e267b0c98868d0aaf7c
IliaValkov <ilia.valkov97@gmail.com>
2020-10-21 20:23:11 +0200

    Added heading to the form component

--- app/client/src/code/components/LoginView.tsx
+++ app/client/src/code/components/LoginView.tsx
@@ -2,9 +2,10 @@ import React from "react";
 
 export default function LoginView ({state}) {
     return(
-        <div className="loginForm">
-            <button onClick={state.handleGoogleLogin}>Login with Google</button>
-            <button onClick={state.proceedWithoutLogin}>Proceed without login</button>
+        <div className="loginForm surface elementLevel1">
+            <h3>Login</h3>
+            <button onClick={state.handleGoogleLogin} >Login with Google</button>
+            <button onClick={state.proceedWithoutLogin} >Proceed without login</button>
         </div>
     );
 }
