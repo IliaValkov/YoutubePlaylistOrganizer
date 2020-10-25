0d5695eefe6dcfa7c37c43be992c48342d93c5b6
IliaValkov <ilia.valkov97@gmail.com>
2020-10-25 18:42:42 +0100

    Finalized Login View prototype

--- app/client/src/code/components/LoginView.tsx
+++ app/client/src/code/components/LoginView.tsx
@@ -1,11 +1,23 @@
 import React from "react";
+import Button from "./Button"
 
 export default function LoginView ({state}) {
+    
+    const googleRed = "#DB4436";
+    
     return(
         <div className="loginForm surface elementLevel1">
             <h2>Login</h2>
-            <button onClick={state.handleGoogleLogin} >Login with Google</button>
-            <button onClick={state.proceedWithoutLogin} >Proceed without login</button>
+            <Button 
+                style = {{"background-color" : googleRed, "color" : "#FFFFFF"}} 
+                onClick={state.handleGoogleLogin} >
+                Login with Google
+            </Button>
+            <Button 
+                disabled={false}
+                onClick={state.proceedWithoutLogin} >
+                Proceed without login
+            </Button>
         </div>
     );
 }
