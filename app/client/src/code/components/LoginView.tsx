eee82a573149e549a5d67183b823737eb7759e50
IliaValkov <ilia.valkov97@gmail.com>
2020-10-25 17:20:37 +0100

    Adding fonts to the app

--- app/client/src/code/components/LoginView.tsx
+++ app/client/src/code/components/LoginView.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function LoginView ({state}) {
     return(
         <div className="loginForm surface elementLevel1">
-            <h3>Login</h3>
+            <h2>Login</h2>
             <button onClick={state.handleGoogleLogin} >Login with Google</button>
             <button onClick={state.proceedWithoutLogin} >Proceed without login</button>
         </div>
